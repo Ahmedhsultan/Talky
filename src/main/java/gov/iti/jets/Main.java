903f42ec60361d864d16a48cd7acfabd1eea61c4
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-01-31 18:45:21 +0200

    fxml errors

--- src/main/java/gov/iti/jets/Main.java
+++ src/main/java/gov/iti/jets/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/Register.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
