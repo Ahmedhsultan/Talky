b23277c8b13b959373711c497884669b8f33fc66
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-02-01 16:56:26 +0200

    add util and userRemote

---
+++ src/main/java/gov/iti/jets/dto/registration/UserRegistrationDto.java
@@ -0,0 +1,18 @@
+package gov.iti.jets.dto.registration;
+
+import gov.iti.jets.dto.BaseDto;
+import gov.iti.jets.dto.UserDto;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class UserRegistrationDto implements BaseDto, Serializable {
+    private UserDto userDto;
+    private String password;
+}
