03434440d5dcb19cd611c4f60b170f56cb3cfaa7
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-02-04 00:41:19 +0200

    Error when saving Image profile

--- src/main/java/gov/iti/jets/Main.java
+++ src/main/java/gov/iti/jets/Main.java
@@ -4,13 +4,19 @@ package gov.iti.jets;
 import gov.iti.jets.dto.UserDto;
 import gov.iti.jets.dto.registration.UserRegistrationDto;
 import gov.iti.jets.network.service.RegisterService;
+import gov.iti.jets.util.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 public class Main  {
@@ -28,14 +34,29 @@ public class Main  {
 //        stage.show();
 //    }
     public static void main(String[] args) {
-
 //        launch();
         RegisterService reg;
         reg = new RegisterService();
         UserDto user = new UserDto();
         user.setId("01078965432");
+        user.setImgPath("01078965432.png");
+        try {
+            user.setImage(imageToByteArray("C:/Users/hp/Pictures/Camera Roll/test.png"));
+            System.out.println("success");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         user.setName("Amany");
         UserRegistrationDto x = new UserRegistrationDto(user,"Amany12345");
         reg.addUser(x);
+    }
+    public static byte[] imageToByteArray(String path) throws IOException
+    {
+        BufferedImage bImage = ImageIO.read(new File(path));
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        String tokens[] = path.split("[.]",0);
+        ImageIO.write(bImage, tokens[tokens.length-1], bos );
+        byte [] data = bos.toByteArray();
+        return data;
     }
 }
--- src/main/java/gov/iti/jets/network/UserRemote.java
+++ src/main/java/gov/iti/jets/network/UserRemote.java
@@ -1,17 +1,14 @@
 package gov.iti.jets.network;
 
-import gov.iti.jets.dto.registration.UserRegistrationDto;
 import gov.iti.jets.dto.UserSessionDto;
+import gov.iti.jets.dto.registration.UserRegistrationDto;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface UserRemote extends Remote {
-
     public void setOnlineStatus(String phone,String status) throws RemoteException;
     public UserSessionDto register(UserRegistrationDto userRegistrationDto) throws RemoteException;
     public UserSessionDto login(String phone, String password) throws RemoteException;
     public void logout(String phone) throws RemoteException;
-
-
 }
