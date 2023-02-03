fbee4096788de1f5ded50f17edbdbd5289a78573
ahmedhsultan <a.hossam@kemet.com.eg>
2023-02-03 02:40:43 +0200

    Notification entity and dto

---
+++ src/main/java/gov/iti/jets/entity/Notification.java
@@ -0,0 +1,21 @@
+package gov.iti.jets.entity;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Notification implements BaseEntity{
+    private int id;
+    private int sender_id;
+    private int receiver_id;
+    private String type;
+    private Date created_on;
+    private String status;
+    private boolean is_seen;
+}
