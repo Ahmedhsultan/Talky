bebd3c2ab59bb826add700f97febc7696b8ecc40
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-02-04 20:47:58 +0200

    change Password pattern

--- src/main/java/gov/iti/jets/util/Constants.java
+++ src/main/java/gov/iti/jets/util/Constants.java
@@ -17,9 +17,14 @@ public class Constants {
 
     //Validation Patterns
     public static final String PHONE_PATTERN = "^[01][0-9]{10}$";
-    public static final String PASSWORD_PATTERN = "([a-zA-Z0-9]){8,20}$";
+    public static final String PASSWORD_PATTERN = "^(?=.*[0-9])"
+            + "(?=.*[a-z])(?=.*[A-Z])"
+            + "(?=.*[@#$%^&+=])"
+            + "(?=\\S+$).{8,20}$";
     public static final String NAME_PATTERN = "^[a-zA-Z ]{3,20}$";
     public static final String EMAIL_PATTERN ="^(.+)@(\\S+)$";
+    public static final String DATE_REGXP = "^\\d{4}-\\d{2}-\\d{2}$";
+
 
 
     //Online status if he is not offline then he is online
