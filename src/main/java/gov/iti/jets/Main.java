a62a43904c287da84569851f9df57f4d3b424cca
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-02-04 23:20:01 +0200

    solve conflict main

--- src/main/java/gov/iti/jets/Main.java
+++ src/main/java/gov/iti/jets/Main.java
@@ -28,57 +28,55 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-<<<<<<< HEAD
-public class Main  {
-//    @Override
-//    public void start(Stage stage) throws IOException {
+//public class Main  {
+////    @Override
+////    public void start(Stage stage) throws IOException {
+////
+////        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+////        double width = screenSize.getWidth();
+////        double height = screenSize.getHeight() - 60;
+////        Parent root = FXMLLoader.load(getClass().getResource("/views/Register.fxml"));
+//////        Parent root = FXMLLoader.load(getClass().getResource("/views/MainScene.fxml"));
+////        stage.setTitle("Registration");
+////        stage.setResizable(false);
+////        stage.setScene(new Scene(root, width, height));
+////        stage.show();
+////    }
+//    public static void main(String[] args) {
+////        launch();
+////        RegisterService reg;
+////        reg = new RegisterService();
+//        UserDto user = new UserDto();
+//        user.setId("01078965432");
+////        user.setImgPath("01078965432.png");
+////        try {
+////            user.setImage(imageToByteArray("C:/Users/hp/Pictures/Camera Roll/test.png"));
+////            System.out.println("success");
+////        } catch (IOException e) {
+////            throw new RuntimeException(e);
+////        }
+//        user.setName("Amany");
+////        UserRegistrationDto x = new UserRegistrationDto(user,"Amany12345");
+////        reg.addUser(x);
 //
-//        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-//        double width = screenSize.getWidth();
-//        double height = screenSize.getHeight() - 60;
-//        Parent root = FXMLLoader.load(getClass().getResource("/views/Register.fxml"));
-////        Parent root = FXMLLoader.load(getClass().getResource("/views/MainScene.fxml"));
-//        stage.setTitle("Registration");
-//        stage.setResizable(false);
-//        stage.setScene(new Scene(root, width, height));
-//        stage.show();
-//    }
-    public static void main(String[] args) {
-//        launch();
-//        RegisterService reg;
-//        reg = new RegisterService();
-        UserDto user = new UserDto();
-        user.setId("01078965432");
-//        user.setImgPath("01078965432.png");
+//        Registry registry = null;
 //        try {
-//            user.setImage(imageToByteArray("C:/Users/hp/Pictures/Camera Roll/test.png"));
-//            System.out.println("success");
-//        } catch (IOException e) {
+//            registry = LocateRegistry.getRegistry("localhost",1099);
+//            IConnection obj = (IConnection) registry.lookup("connection");
+//            ConnectionDto connectionDto = new ConnectionDto();
+//            connectionDto.setUserDto(user);
+//            connectionDto.setIClient(new CheckConnection());
+//            obj.connect(connectionDto);
+//            while (true){
+//                Thread.sleep(40000);
+//            }
+//        } catch (RemoteException e) {
+//            throw new RuntimeException(e);
+//        } catch (NotBoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (InterruptedException e) {
 //            throw new RuntimeException(e);
 //        }
-        user.setName("Amany");
-//        UserRegistrationDto x = new UserRegistrationDto(user,"Amany12345");
-//        reg.addUser(x);
-
-        Registry registry = null;
-        try {
-            registry = LocateRegistry.getRegistry("localhost",1099);
-            IConnection obj = (IConnection) registry.lookup("connection");
-            ConnectionDto connectionDto = new ConnectionDto();
-            connectionDto.setUserDto(user);
-            connectionDto.setIClient(new CheckConnection());
-            obj.connect(connectionDto);
-            while (true){
-                Thread.sleep(40000);
-            }
-        } catch (RemoteException e) {
-            throw new RuntimeException(e);
-        } catch (NotBoundException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-=======
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -92,26 +90,8 @@ public class Main extends Application {
         stage.setResizable(false);
         stage.setScene(new Scene(root, width, height));
         stage.show();
->>>>>>> feature_rmi
     }
     public static void main(String[] args) {
         launch();
-//        RegisterService reg;
-//        reg = new RegisterService();
-//        UserDto user = new UserDto();
-//        user.setId("01090780888");
-//        user.setImgPath("01078965432.png");
-//
-//        try {
-//
-//            user.setImage(imageToByteArray("C:\\Users\\hp\\Pictures\\test.png"));
-//            System.out.println("success ");
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        user.setName("Dina");
-//        UserRegistrationDto x = new UserRegistrationDto(user,"Amany12345");
-//        reg.addUser(x);
-//    }
     }
 }
