84551ae3b9d162198edba8b0e0a45613b46ef015
ahmedhsultan <a.hossam@kemet.com.eg>
2023-02-13 00:11:56 +0200

    add memberlist to ChatDto

--- common/src/main/java/gov/iti/jets/common/dto/UserCardDto.java
+++ common/src/main/java/gov/iti/jets/common/dto/UserCardDto.java
@@ -3,13 +3,15 @@ package gov.iti.jets.common.dto;
 import gov.iti.jets.common.dto.Interfaces.IUser;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @ToString
-public class UserCardDto extends IUser  implements  BaseDto {
+public class UserCardDto extends IUser  implements  BaseDto, Serializable {
     private String id;
     private String name;
     private String imgPath;
